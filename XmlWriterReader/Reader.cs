fc2e95eecb99acd07d21f8887bfa47e59a959195
andriisytnyk <37737967+andriisytnyk@users.noreply.github.com>
2018-09-27 14:58:58 +0300

    Rebuilt .dlls

--- XmlWriterReader/Reader.cs
+++ XmlWriterReader/Reader.cs
@@ -4,21 +4,12 @@ using System.Xml.Serialization;
 
 namespace XmlWriterReader
 {
-    /// <summary>
-    /// Class for working with files.
-    /// </summary>
-    /// <typeparam name="T"></typeparam>
     public static class XmlReader<T>
     {
         private static List<T> _list = new List<T>();
 
         public static bool condition = true;
    
-        /// <summary>
-        /// Read all T-type items from file.
-        /// </summary>
-        /// <param name="fileName">Specifies the file name.</param>
-        /// <returns>List of T-type items</returns>
         public static List<T> Read(string fileName)
         {
             var formatter = new XmlSerializer(typeof(List<T>));
